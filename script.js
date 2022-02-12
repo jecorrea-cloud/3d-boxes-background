3e80bc8b7dc597e6f471f93193a29255ef38f58b
joaquin <jecorrea@protonmail.com>
2022-02-11 19:58:10 -0500

    full functionality & full HTML

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 // Bring down the boxes container and the button
 const boxesContainer = document.getElementById("boxes");
 const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      // Construct those 16 boxes
+      const box = document.createElement("div");
+      //   console.log(box);
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxesContainer.appendChild(box);
+    }
+  }
+}
+
+createBoxes();
